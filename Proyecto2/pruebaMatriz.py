48b0e6b7a5c22f32eef51ac87c00615614a13671
CDCS1002 <carlos.daniel.calderon.cdcs@gmail.com>
2020-10-27 18:24:39 -0600

    pruebas

--- Proyecto2/pruebaMatriz.py
+++ Proyecto2/pruebaMatriz.py
@@ -11,102 +11,177 @@ Matriz=[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-def Norte(Coords, Matriz):
-	try:
-		if Matriz[Coords[0]][Coords[1]]==1:
-			return True
-		elif Coords[0]>=0:
-			return Norte((Coords[0]-1, Coords[1]), Matriz)
-		else:
-			return False
-	except (IndexError):
-		return False 
+        
+MatrizPixeles=[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	    	   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		       [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+		       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-def Sur(Coords, Matriz):
+def Norte(Coords, Matriz, Tiempo):
 	try:
-		if Matriz[Coords[0]][Coords[1]]==1:
-			return True
-		else:
-			return Sur((Coords[0]+1, Coords[1]), Matriz)
-	except (IndexError):
-		return False
-
-def Este(Coords, Matriz):
-	try:
-		if Matriz[Coords[0]][Coords[1]]==1:
-			return True
-		else:
-			return Este((Coords[0], Coords[1]+1), Matriz)
-	except (IndexError):
-		return False
-
-def Oeste(Coords, Matriz):
-	try:
-		if Matriz[Coords[0]][Coords[1]]==1:
-			return True
-		elif Coords[1]>=0 and Coords[0]>=0:
-			return Oeste((Coords[0], Coords[1]-1), Matriz)
+		if Coords[1]>=0 and Coords[0]>=0:
+			if Matriz[Coords[0]][Coords[1]]==1:
+				return Tiempo
+			else:
+				return Norte((Coords[0]-1, Coords[1]), Matriz, Tiempo+1)
 		else:
 			return False
 	except (IndexError):
 		return False
 
-def NorEste(Coords, Matriz):
+def Sur(Coords, Matriz, Tiempo):
 	try:
-		if Matriz[Coords[0]][Coords[1]]==1:
-			return True
-		else:
-			return NorEste((Coords[0]-1, Coords[1]+1), Matriz)
-	except (IndexError):
-		return False
-
-def SurEste(Coords, Matriz):
-	try:
-		if Matriz[Coords[0]][Coords[1]]==1:
-			return True
-		else:
-			return SurEste((Coords[0]+1, Coords[1]+1), Matriz)
-	except (IndexError):
-		return False
-
-def SurOeste(Coords, Matriz):
-	try:
-		if Matriz[Coords[0]][Coords[1]]==1:
-			return True
-		else:
-			return SurOeste((Coords[0]+1, Coords[1]-1), Matriz)
-	except (IndexError):
-		return False
-
-def NorOeste(Coords, Matriz):
-	try:
-		if Matriz[Coords[0]][Coords[1]]==1:
-			return True
-		elif Coords[0]>=0 and Coords[1]>=0:
-			return NorOeste((Coords[0]-1, Coords[1]-1), Matriz)
+		if Coords[1]>=0 and Coords[0]>=0:
+			if Matriz[Coords[0]][Coords[1]]==1:
+				return Tiempo
+			else:
+				return Sur((Coords[0]+1, Coords[1]), Matriz, Tiempo+1)
 		else:
 			return False
 	except (IndexError):
 		return False
+
+def Este(Coords, Matriz, Tiempo):
+	try:
+		if Coords[1]>=0 and Coords[0]>=0:
+			if Matriz[Coords[0]][Coords[1]]==1:
+				return Tiempo
+			else:
+				return Este((Coords[0], Coords[1]+1), Matriz, Tiempo+1)
+		else:
+			return False
+	except (IndexError):
+		return False
+
+def Oeste(Coords, Matriz, Tiempo):
+	try:
+		if Coords[1]>=0 and Coords[0]>=0:
+			if Matriz[Coords[0]][Coords[1]]==1:
+				return Tiempo
+			else:
+				return Oeste((Coords[0], Coords[1]-1), Matriz, Tiempo+1)
+		else:
+			return False
+	except (IndexError):
+		return False
+
+def NorEste(Coords, Matriz, Tiempo):
+	try:
+		if Coords[1]>=0 and Coords[0]>=0:
+			if Matriz[Coords[0]][Coords[1]]==1:
+				return Tiempo
+			else:
+				return NorEste((Coords[0]-1, Coords[1]+1), Matriz, Tiempo+1)
+		else:
+			return False
+	except (IndexError):
+		return False
+
+def SurEste(Coords, Matriz, Tiempo):
+	try:
+		if Coords[1]>=0 and Coords[0]>=0:
+			if Matriz[Coords[0]][Coords[1]]==1:
+				return Tiempo
+			else:
+				return SurEste((Coords[0]+1, Coords[1]+1), Matriz, Tiempo+1)
+		else:
+			return False
+	except (IndexError):
+		return False
+
+def SurOeste(Coords, Matriz, Tiempo):
+	try:
+		if Coords[1]>=0 and Coords[0]>=0:
+			if Matriz[Coords[0]][Coords[1]]==1:
+				return Tiempo
+			else:
+				return SurOeste((Coords[0]+1, Coords[1]-1), Matriz, Tiempo+1)
+		else:
+			return False
+	except (IndexError):
+		return False
+
+def NorOeste(Coords, Matriz, Tiempo):
+	try:
+		if Coords[1]>=0 and Coords[0]>=0:
+			if Matriz[Coords[0]][Coords[1]]==1:
+				return Tiempo
+			else:
+				return NorOeste((Coords[0]-1, Coords[1]-1), Matriz, Tiempo+1)
+		else:
+			return False
+	except (IndexError):
+		return False
+
+def DNorte(fila, columna):
+	tiempo=Norte((fila, columna), Matriz, 0)
+	if tiempo!=False:
+		MatrizPixeles[fila-tiempo][columna]=1
+
+
+def DSur(fila, columna):
+	tiempo=Sur((fila, columna), Matriz, 0)
+	if tiempo!=False:
+		MatrizPixeles[fila+tiempo][columna]=1
+
+def DEste(fila, columna):
+	tiempo=Este((fila, columna), Matriz, 0)
+	if tiempo!=False:
+		MatrizPixeles[fila][columna+tiempo]=1
+
+def DOeste(fila, columna):
+	tiempo=Oeste((fila, columna), Matriz, 0)
+	if tiempo!=False:
+		MatrizPixeles[fila-tiempo][columna-tiempo]=1
+
+def DNorEste(fila, columna):
+	tiempo=NorEste((fila, columna), Matriz, 0)
+	if tiempo!=False:
+		MatrizPixeles[fila-tiempo][columna+tiempo]=1
+
+def DSurEste(fila, columna):
+	tiempo=SurEste((fila, columna), Matriz, 0)
+	if tiempo!=False:
+		MatrizPixeles[fila+tiempo][columna+tiempo]=1
+
+def DNorOeste(fila, columna):
+	tiempo=NorOeste((fila, columna), Matriz, 0)
+	if tiempo!=False:
+		MatrizPixeles[fila-tiempo][columna-tiempo]=1
+
+def DSurOeste(fila, columna):
+	tiempo=SurOeste((fila, columna), Matriz, 0)
+	if tiempo!=False:
+		MatrizPixeles[fila+tiempo][columna-tiempo]=1
 
 def Fabric(Matriz):
 	num=randint(1, 8)
 	switch={
-		1:Norte((5, 1), Matriz),
-		2:Sur((5, 1), Matriz),
-		3:Este((5, 1), Matriz),
-		4:Oeste((5, 1), Matriz),
-		5:NorEste((5, 1), Matriz),
-		6:SurEste((5, 1), Matriz),
-		7:SurOeste((5, 1), Matriz),
-		8:NorOeste((5, 1), Matriz)
+		1:DNorte(5,1),
+		2:DSur(5,1),
+		3:DEste(5,1),
+		4:DOeste(5,1),
+		5:DNorEste(5,1),
+		6:DSurEste(5,1),
+		7:DSurOeste(5,1),
+		8:DNorOeste(5,1)
 	}
-	print(num)
 	return switch[num]
 
 def Algoritmo(Matriz):
 	for i in range(0, 10):
-		print(Fabric(Matriz))
+		Fabric(Matriz)
+	for j in MatrizPixeles:
+		print(j)
+
+
+
 
 
 
